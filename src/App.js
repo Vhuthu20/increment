6810f0d7618932b2b6e770e28ad63fdf01657ae0
Vhuthu20 <402418737@my.richfield.ac.za>
2025-10-16 11:59:59 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Counter } from "./Counter";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello CodeSandbox</h1>
+      <h2>My React</h2>
+      <Counter />
+    </div>
+  );
+}
---
+++ src/Counter.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+export function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h2>Counter:{count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+}
